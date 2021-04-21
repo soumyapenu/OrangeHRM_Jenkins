b19af3e9d731f70eeed6011d00f9a0195dd0b6c7
soumyapenu <soumyapenu@gmail.com>
2021-04-22 00:29:54 +0530

    error fix

--- src/main/java/com/ExtentReport/Listener.java
+++ src/main/java/com/ExtentReport/Listener.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
-import com.OrangeHRM.BaseClass;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
